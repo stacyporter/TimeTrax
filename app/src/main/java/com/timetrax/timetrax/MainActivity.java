5bc0b7e1fedeb17b60380c4ab4e0a93a38ebacc7
Stacy Porter <mr.stacy.porter@gmail.com>
2016-03-03 00:11:34 +1000

    Got the buttons sorted

--- app/src/main/java/com/timetrax/timetrax/MainActivity.java
+++ app/src/main/java/com/timetrax/timetrax/MainActivity.java
@@ -2,12 +2,28 @@ package com.timetrax.timetrax;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.Timer;
 
 public class MainActivity extends AppCompatActivity {
+
+    long timer1 = 0;
+    long timer2 = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void startTimer1(View view) {
+    }
+
+    public void startTimer2(View view) {
+    }
+
 }
